5bb146ff14c07c30dd0a95cc8e779e36199c549a
Matt Filetto <matt.filetto@gmail.com>
2023-01-26 23:47:41 -0800

    gs201: import Face Enroll rro_overlays
    * Fixes Face Unlock Setup
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

---
+++ spark_common.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+PRODUCT_PACKAGES += \
+    FaceEnrollSettingsOverlay \
+    FaceEnrollOverlay
+
