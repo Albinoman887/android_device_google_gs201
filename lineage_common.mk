b3cf00af83d67129307b9038d6692060a7c099f0
Matt Filetto <matt.filetto@gmail.com>
2023-05-18 00:53:10 -0700

    gs201: Initial AlphaDroid bringup
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-banana
+DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-lineage
 
 # Adaptive charging
 PRODUCT_COPY_FILES += \
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-    
+
 # Quick Tap
 PRODUCT_COPY_FILES += \
     device/google/gs201/conf/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
