382f9712e0c650acee9a078b6cbd419a52ee84f9
Matt Filetto <matt.filetto@gmail.com>
2023-06-19 23:01:58 -0700

    gs201: Revert the BananaDroid branding
    * Rename for easy forking for device bringup. it was all
     just cosmetic filename changes anyways. Keeping the Lineage
     string will make merging commits easier if we all commit to
     keeping that naming convention
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-banana
+DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-lineage
 
 # Adaptive charging
 PRODUCT_COPY_FILES += \
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-    
+
 # Quick Tap
 PRODUCT_COPY_FILES += \
     device/google/gs201/conf/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
@@ -52,5 +52,5 @@ include hardware/google/pixel/lineage_health/device.mk
 PRODUCT_PACKAGES += \
     GoogleParts
 
-# Touch
-include hardware/google/pixel/touch/device.mk
+# Build Lineage Touch HAL if it exists
+-include hardware/google/pixel/touch/device.mk
